fbf238baef5d03a8be3f4ce29668d3a16dc9d007
Manika Agarwal <manikaag29@gmail.com>
2026-05-27 18:31:26 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.54%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        int a = 1;
+        int b = 1;
+        int c = 0;
+
+        while(n > 1){
+
+            c = a + b;
+            a = b;
+            b = c;
+
+            n--;
+        }
+
+        return c;
+    }
+}
