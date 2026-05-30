b66e357f44edd5e63e00053be69364beaf9aa88d
Manika Agarwal <manikaag29@gmail.com>
2026-05-30 19:18:11 +0530

    Time: 1 ms (21.06%), Space: 43.5 MB (58.23%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void sortColors(int[] a) {
+        int n=a.length;
+        for(int i=0;i<n-1;i++){
+            int min=i;
+            for(int j=i+1;j<n;j++){
+                if(a[min]>a[j]){
+                    min=j;
+                }
+            }
+            int temp=a[i];
+            a[i]=a[min];
+            a[min]=temp;
+        }
+        
+    }
+}
