1476bb477f87b072144c6a4fe23e70a3a0b08f48
Manika Agarwal <manikaag29@gmail.com>
2026-08-23 21:23:44 +0530

    Time: 10 ms (16.06%), Space: 46.9 MB (64.23%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+
+        int sum1 = 0, sum2 = 0;
+        int q1 = 0, q2 = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?')
+                q1++;
+            else
+                sum1 += num.charAt(i) - '0';
+
+            if (num.charAt(n - 1 - i) == '?')
+                q2++;
+            else
+                sum2 += num.charAt(n - 1 - i) - '0';
+        }
+
+        int diff = sum1 - sum2;
+        int qDiff = q1 - q2;
+
+        if (qDiff % 2 != 0)
+            return true;
+
+        return diff != -9 * (qDiff / 2);
+    }
+}
